b677b744a45059ec8fcb2da90a8eae9d1a31c7d4
Shuoshuo <liqiangge628@gmail.com>
2022-06-01 16:33:25 +0800

    day0601

---
+++ day0601es6AndJquery/js/test.js
@@ -0,0 +1,8 @@
+// 向外导出多个变量
+export default {
+    name: '硕硕',
+    age: 21,
+    show() {
+        console.log('我是test.js中的show()方法')
+    }
+}
---
+++ day0601es6AndJquery/js/test_friend.js
@@ -0,0 +1,6 @@
+// 导入js
+import Test from '../js/test.js'
+
+console.log(Test.name)
+console.log(Test.age)
+Test.show()
